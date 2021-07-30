df5adb46f24f6d2ea26cb8da94281c482aca2de7
Caio Farias <caio4095@gmail.com>
2021-07-30 01:26:20 -0300

    hotfix - usersmiddleware

--- src/micservices/users/UsersMiddleware.js
+++ src/micservices/users/UsersMiddleware.js
@@ -2,8 +2,7 @@ const { getServerURL } = require('../../utils')
 
 module.exports = {
   setAvatar( req, res, next){
-    console.log(req.file)
-    const avatar = getServerURL() + '/uploads/user-avatars/' +  + req.file.filename
+    const avatar = getServerURL() + '/uploads/user-avatars/' + req.file.filename
     req.body =  {...req.body, avatar}
     next()
   }
