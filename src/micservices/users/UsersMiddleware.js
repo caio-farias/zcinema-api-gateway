91ab5e40f0ee93b7887d9a1fe3be6d4e84e5cca5
Caio Farias <caio4095@gmail.com>
2021-08-03 16:04:41 -0300

    FIx setavatr middleware

--- src/micservices/users/UsersMiddleware.js
+++ src/micservices/users/UsersMiddleware.js
@@ -2,8 +2,9 @@ const { getServerURL } = require('../../utils')
 
 module.exports = {
   setAvatar( req, res, next){
+    console.log(req.file != undefined)
     const avatar = getServerURL() + '/uploads/user-avatars/' 
-    + (req.file =! undefined ?  req.file.filename : 'placeholder.png')
+      + (req.file == undefined ?  'placeholder.png' : req.file.filename)
     req.body =  {...req.body, avatar}
     next()
   }
