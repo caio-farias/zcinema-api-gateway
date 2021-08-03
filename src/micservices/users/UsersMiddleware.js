c6c4e67efdb200c90f04b1678f4cfd20950d0bbd
Caio Farias <caio4095@gmail.com>
2021-08-03 15:52:33 -0300

    Hotfix - add avatar placeholder

--- src/micservices/users/UsersMiddleware.js
+++ src/micservices/users/UsersMiddleware.js
@@ -2,7 +2,8 @@ const { getServerURL } = require('../../utils')
 
 module.exports = {
   setAvatar( req, res, next){
-    const avatar = getServerURL() + '/uploads/user-avatars/' + req.file.filename
+    const avatar = getServerURL() + '/uploads/user-avatars/' 
+    + (req.file.filename =! undefined ?  req.file.filename : 'placeholder.png')
     req.body =  {...req.body, avatar}
     next()
   }
