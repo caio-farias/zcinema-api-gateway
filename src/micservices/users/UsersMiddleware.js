885d03550f75e3c1a08d20c9c8e95df372099c97
Caio Farias <caio4095@gmail.com>
2021-08-03 15:56:16 -0300

    Hotfix - set avatar middleware

--- src/micservices/users/UsersMiddleware.js
+++ src/micservices/users/UsersMiddleware.js
@@ -3,7 +3,7 @@ const { getServerURL } = require('../../utils')
 module.exports = {
   setAvatar( req, res, next){
     const avatar = getServerURL() + '/uploads/user-avatars/' 
-    + (req.file.filename =! undefined ?  req.file.filename : 'placeholder.png')
+    + (req.file =! undefined ?  req.file.filename : 'placeholder.png')
     req.body =  {...req.body, avatar}
     next()
   }
