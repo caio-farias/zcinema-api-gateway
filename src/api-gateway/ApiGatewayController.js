ab07bb95e84d3b83e5a5e96a315de1dcc4510d8b
Caio Farias <caio4095@gmail.com>
2021-08-03 13:55:35 -0300

    Develop (#4)
    * Feature/bookings micservice (#3)

--- src/api-gateway/ApiGatewayController.js
+++ src/api-gateway/ApiGatewayController.js
@@ -1,14 +1,51 @@
 const axios = require('axios')
-const { getMicServiceURL } = require('../utils')
+const { getMicServiceURL, isRedundancyMethod, shouldApplyRedundancy } = require('../utils')
 const { stringify } = require('querystring')
 const { secret } = require('../micserviceSecret.json')
+
+const applyRedundancy = async (micserviceName, redundancyService, req, res) => {
+  const { reqPath } = req
+  const micserviceUrl = getMicServiceURL(redundancyService)
+    + `${reqPath != undefined ? `/${micserviceName}/` + reqPath : `/${micserviceName}`}`
+  const query =  Object.keys(req.query).length > 0 ? ('?' + stringify(req.query)) : ''
+  try {
+
+    if(micserviceName == 'users'){
+      delete req.body.password
+      delete req.body.avatar
+    } else if(micserviceName == 'movies'){
+      delete req.body.banner
+      delete req.body.trailer
+    }
+    console.log(micserviceUrl)
+    const micserviceResponse = await axios({
+      method: req.method,
+      url: micserviceUrl + query,
+      headers: {
+        'Authorization': secret,
+        'Content-Type': 'application/json',
+      },
+      data: req.body,
+      timeout: 3000,
+    })
+    const body = micserviceResponse.data
+    console.log(body) 
+  } catch (error) {
+    console.log(error)
+    const { message } = error.response.data || undefined
+    return res.status(400).json({ 
+      message : message || "Ocorreu um erro neste microsserviço, tente novamente."
+    })
+  }
+}
 
 module.exports = {
   async passFoward (req, res){
     const { micserviceName, reqPath } = req
     const micserviceUrl = getMicServiceURL(micserviceName) 
      + `${reqPath != undefined ? '/' + reqPath : ''}`
-    const query =  req.query? '?' + stringify(req.query) : ''
+    console.log(reqPath)
+    const query =  Object.keys(req.query).length > 0  ? ('?' + stringify(req.query)) : ''
     
     try {
       const micserviceResponse = await axios({
@@ -22,14 +59,24 @@ module.exports = {
         timeout: 3000,
         rejectUnauthorized: false
       })
+      
       const body = micserviceResponse.data
+
+      if( 
+        isRedundancyMethod('bookings', req.method) &&
+        shouldApplyRedundancy('bookings', micserviceName)
+        ){
+          await applyRedundancy(micserviceName, 'bookings', req, res)
+      }
+
       return res.json({ ...body })
     } catch (error) {
-      const { message } = error.response.data
+      // console.log(error)
+      const { message } = error.response.data || undefined
       return res.status(400).json({ 
         message : message || "Ocorreu um erro neste microsserviço, tente novamente."
       })
     }
-  }
+  },
 }
 
--- src/server.js
+++ src/server.js
@@ -4,6 +4,7 @@ const corsMiddleware = require('./cors')
 const userRoutes = require('./micservices/users/routes')
 const authRoutes = require('./micservices/auth/routes')
 const moviesRoutes = require('./micservices/movies/routes')
+const bookingsRoutes = require('./micservices/bookings/routes')
 
 require('dotenv/config')
 
@@ -22,5 +23,7 @@ app.use((req, res, next) =>{
 app.use(userRoutes)
 app.use(authRoutes)
 app.use(moviesRoutes)
+app.use(bookingsRoutes)
+
 app.use(express.static('tmp'))
 app.listen(process.env.PORT || 3333)
