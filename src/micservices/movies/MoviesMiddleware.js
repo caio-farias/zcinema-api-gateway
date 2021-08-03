a236fa2d26dea52d6a7fc2e79a3a2b33f8d2ade0
Caio Farias <caio4095@gmail.com>
2021-08-03 20:42:48 -0300

    hotfix - setbanner middleware

--- src/micservices/movies/MoviesMiddleware.js
+++ src/micservices/movies/MoviesMiddleware.js
@@ -2,7 +2,8 @@ const { getServerURL } = require('../../utils')
 
 module.exports = {
   setBanner( req, res, next){
-    const banner = getServerURL() + '/uploads/movie-banners/' + req.file.filename
+    const banner = getServerURL() + '/uploads/movie-banners/'
+    + (req.file == undefined ?  'banner.jpg' : req.file.filename)
     req.body =  {...req.body, banner}
     next()
   }
