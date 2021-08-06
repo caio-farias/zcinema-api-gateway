fe69c9453c17bb715572c54428d80371e0381080
Caio Farias <caio4095@gmail.com>
2021-08-06 08:11:14 -0300

    change timeout

--- src/api-gateway/axios.js
+++ src/api-gateway/axios.js
@@ -1,9 +1,21 @@
 const axiosInstance = require('axios')
 const { secret } = require('../micserviceSecret.json')
 const { getMicServiceURL } = require('../utils')
+const axiosRetry = require('axios-retry')
 
 const baseURL = getMicServiceURL('auth')
 const axios = axiosInstance.create({ baseURL })
+
+axiosRetry(axios, {
+  retries: 3,
+  retryDelay: (retryCount) => {
+    console.log(`retry attempt: ${retryCount}`);
+    return retryCount * 2000;
+  },
+  retryCondition: (error) => {
+    return error.response.status === 503;
+  },
+})
 
 axios.interceptors.request.use(async config => {
 
@@ -15,5 +27,6 @@ axios.interceptors.request.use(async config => {
 
   return { ...config, headers }
 })
+
 
 module.exports = axios
