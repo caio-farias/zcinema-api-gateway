fe21869da7c8ee0af13026a7f02665be4ad9e782
Caio Farias <caio4095@gmail.com>
2021-08-10 20:07:40 -0300

    Feature/ added micservice wake up method to api gateway

--- src/api-gateway/ApiGatewayController.js
+++ src/api-gateway/ApiGatewayController.js
@@ -1,15 +1,18 @@
-const axios = require('axios')
+const axios = require('axios').create()
 const { getMicServiceURL, isRedundancyMethod, shouldApplyRedundancy } = require('../utils')
 const { stringify } = require('querystring')
 const { secret } = require('../micserviceSecret.json')
+const axiosRetry = require('axios-retry')
 
-const applyRedundancy = async (micserviceName, redundancyService, req, res) => {
+const applyRedundancy = async (micserviceName, redundancyService, redundancyId, req, res) => {
   const { reqPath } = req
   const micserviceUrl = getMicServiceURL(redundancyService)
     + `${reqPath != undefined ? `/${micserviceName}/` + reqPath : `/${micserviceName}`}`
   const query =  Object.keys(req.query).length > 0 ? ('?' + stringify(req.query)) : ''
+
   try {
 
+    req.body.id = redundancyId
     if(micserviceName == 'users'){
       delete req.body.password
       delete req.body.avatar
@@ -17,8 +20,19 @@ const applyRedundancy = async (micserviceName, redundancyService, req, res) => {
       delete req.body.banner
       delete req.body.trailer
     }
-    console.log(micserviceUrl)
-    const micserviceResponse = await axios({
+
+    axiosRetry(axios, {
+      retries: 3,
+      retryDelay: (retryCount) => {
+        console.log(`retry attempt: ${retryCount}`);
+        return retryCount * 2000;
+      },
+      retryCondition: (error) => {
+        return error.response.status === 503;
+      },
+    })
+    
+    await axios({
       method: req.method,
       url: micserviceUrl + query,
       headers: {
@@ -26,16 +40,30 @@ const applyRedundancy = async (micserviceName, redundancyService, req, res) => {
         'Content-Type': 'application/json',
       },
       data: req.body,
-      timeout: 3000,
+      timeout: 5000,
     })
-    const body = micserviceResponse.data
-    console.log(body) 
+
   } catch (error) {
-    console.log(error)
     const { message } = error.response.data || undefined
     return res.status(400).json({ 
       message : message || "Ocorreu um erro neste microsserviço, tente novamente."
     })
+  }
+}
+
+const  testEndpoints = async(micserviceName, req, res) => {
+  const hosts = [getMicServiceURL(micserviceName)]
+  if(
+    isRedundancyMethod('bookings', req.method) &&
+    shouldApplyRedundancy('bookings', micserviceName)
+    )
+      hosts.push(getMicServiceURL('bookings'))
+  try {
+    for(let host of hosts){
+      let res = await axios.get(host, { timeout: 5000 });
+    }    
+  } catch (error) {
+    console.log('VERFYING >> ', error) 
   }
 }
 
@@ -44,10 +72,12 @@ module.exports = {
     const { micserviceName, reqPath } = req
     const micserviceUrl = getMicServiceURL(micserviceName) 
      + `${reqPath != undefined ? '/' + reqPath : ''}`
-    console.log(reqPath)
     const query =  Object.keys(req.query).length > 0  ? ('?' + stringify(req.query)) : ''
     
+    
     try {
+      await testEndpoints(micserviceName, req, res)
+      
       const micserviceResponse = await axios({
         method: req.method,
         url: micserviceUrl + query,
@@ -56,22 +86,28 @@ module.exports = {
           'Content-Type': 'application/json',
         },
         data: req.body,
-        timeout: 3000,
-        rejectUnauthorized: false
+        timeout: 5000,
       })
-      
-      const body = micserviceResponse.data
 
-      if( 
+      if(micserviceResponse.data == undefined)
+        return res.status(503).json({ message: "Heroku botou API Gateway para dormir"})
+
+      const body = micserviceResponse.data
+      if(
         isRedundancyMethod('bookings', req.method) &&
         shouldApplyRedundancy('bookings', micserviceName)
         ){
-          await applyRedundancy(micserviceName, 'bookings', req, res)
+          await applyRedundancy(micserviceName, 'bookings', body.id, req, res)
       }
 
       return res.json({ ...body })
     } catch (error) {
-      // console.log(error)
+      console.log(error)
+      if(error.response == undefined)
+        return res.status(400).json({ 
+          message: "Ocorreu um erro neste microsserviço, tente novamente." 
+        })
+
       const { message } = error.response.data || undefined
       return res.status(400).json({ 
         message : message || "Ocorreu um erro neste microsserviço, tente novamente."
--- src/micservices/users/UsersMiddleware.js
+++ src/micservices/users/UsersMiddleware.js
@@ -2,7 +2,6 @@ const { getServerURL } = require('../../utils')
 
 module.exports = {
   setAvatar( req, res, next){
-    console.log(req.file != undefined)
     const avatar = getServerURL() + '/uploads/user-avatars/' 
       + (req.file == undefined ?  'placeholder.png' : req.file.filename)
     req.body =  {...req.body, avatar}
