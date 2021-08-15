ce52f8aba8f1322e8b062f69fff94d49f0e3dcd5
Caio Farias <caio4095@gmail.com>
2021-08-15 18:59:48 -0300

    Feature/bookings sales integration (#8)

--- src/micservices/sales/SalesMiddleware.js
+++ src/micservices/sales/SalesMiddleware.js
@@ -1,30 +1,58 @@
 const axios = require('./axios')
 
-module.exports = async (req, res, next) => {
-  const { booking_id, user_id, card_id } = req.params
-
-  if(booking_id == undefined)
-    return res.status(400).json({ message: "Informe o bookind_id. " })
+module.exports = {
+  async checkBooking (req, res, next){
+    const { booking_id, user_id } = req.params
   
-    if(user_id == undefined)
-    return res.status(400).json({ message: "Informe o user_id. " })
-
-  
-  try {
-    const result = await axios.get(`/${user_id}/${booking_id}`)
-    const booking  = result.data
-    if(!booking)
-      return res.status(4009).json({ message: "Sessão não existe " })
-
-    next()
+    if(booking_id == undefined)
+      return res.status(400).json({ message: "Informe o bookind_id. " })
     
-  } catch (error) {
-    console.log(error)
-    if(error == undefined)
-      return res.status(500).json({ message: "Ocorreu um erro neste microserviço." })
-    const { message } = error.response.data
-    return res.status(400).json({ 
-      message : message || "Ocorreu um erro neste microsserviço, tente novamente."
-    })
+      if(user_id == undefined)
+      return res.status(400).json({ message: "Informe o user_id. " })
+  
+    
+    try {
+      const result = await axios.get(`/${booking_id}`)
+      const booking  = result.data
+
+      if(!booking)
+        return res.status(4009).json({ message: "Reserva não existe " })
+      if(booking.user_id != user_id)
+        return res.status(4009).json({ message: "Reserva não pertence a este usuário " })
+
+      const price = booking.price
+      const body = req.body
+      req.body = {
+        ...body,
+        price
+      }
+      console.log(req.body)
+      next()
+      
+    } catch (error) {
+      // console.log(error)
+      if(error == undefined)
+        return res.status(500).json({ message: "Ocorreu um erro neste microserviço." })
+      const { message } = error.response.data
+      return res.status(400).json({ 
+        message : message || "Ocorreu um erro neste microsserviço, tente novamente."
+      })
+    }
+  },
+  async confirmSale (req, res){
+    const { booking_id } = req.params
+    try {
+      const result = await axios.post(`/${booking_id}`)
+      const booking = result.data
+      return res.json({ booking })
+    } catch (error) {
+      console.log(error)
+      if(error == undefined)
+        return res.status(500).json({ message: "Ocorreu um erro neste microserviço." })
+      const { message } = error.response.data
+      return res.status(400).json({ 
+        message : message || "Ocorreu um erro neste microsserviço, tente novamente."
+      })
+    }
   }
 }
--- src/micservices/sales/routes.js
+++ src/micservices/sales/routes.js
@@ -4,6 +4,15 @@ const ApiGatewayController = require('../../api-gateway/ApiGatewayController')
 const ApiGatewayMiddleware = require('../../api-gateway/ApiGatewayMiddleware')
 const SalesMiddleware = require('../sales/SalesMiddleware')
 
+
+routes.get(
+  '/api/sales/users/:user_id', 
+  [
+    ApiGatewayMiddleware.verifyRequest,
+    ApiGatewayController.passFoward,
+  ]
+)
+
 routes.post(
   '/api/sales/cards/:user_id', 
   [
@@ -21,11 +30,20 @@ routes.get(
 )
 
 routes.post(
+  '/api/sales/credit/:card_id', 
+  [
+    ApiGatewayMiddleware.verifyRequest,
+    ApiGatewayController.passFoward
+  ]
+)
+
+routes.post(
   '/api/sales/:user_id/:card_id/:booking_id', 
   [
     ApiGatewayMiddleware.verifyRequest,
-    SalesMiddleware, 
-    ApiGatewayController.passFoward
+    SalesMiddleware.checkBooking, 
+    ApiGatewayController.passFowardAsMiddleware,
+    SalesMiddleware.confirmSale
   ]
 )
 routes.get(
