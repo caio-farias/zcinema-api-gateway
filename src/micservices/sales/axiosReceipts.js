83fd556bf569193a1de3b30802687e6f9e7cbef3
Caio Farias <caio4095@gmail.com>
2021-08-15 21:44:52 -0300

    Added receipts create methods

---
+++ src/micservices/sales/axiosReceipts.js
@@ -0,0 +1,19 @@
+const axiosInstance = require('axios')
+const { secret } = require('../../micserviceSecret.json')
+const { getMicServiceURL } = require('../../utils')
+
+const baseURL = getMicServiceURL('receipts')
+const axios = axiosInstance.create({ baseURL })
+
+axios.interceptors.request.use(async config => {
+
+  const headers = { ...config.headers }
+
+  if (secret) {
+    headers.Authorization = secret
+  }
+
+  return { ...config, headers }
+})
+
+module.exports = axios
